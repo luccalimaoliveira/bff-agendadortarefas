4e083e0e96e84998b65832ccba6bd5a8462c9152
Lucca <lucca.lima1984@gmail.com>
2026-05-10 10:53:13 -0300

    Adicionado esqueleto do projeto

---
+++ src/test/java/com/lucca/bff_agendadorTarefas/BffAgendadorTarefasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucca.bff_agendadorTarefas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BffAgendadorTarefasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
